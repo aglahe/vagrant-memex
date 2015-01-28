77eb01a06f1852d41d48958c3aaa92287587f977
Aaron Glahe <aaron.glahe@gmail.com>
2015-01-28 15:09:19 -0500

    Updated to remove spark-alias.sh forgot about that; also seet the log4j to WARN

--- spark/spark-alias.sh
+++
@@ -1,3 +0,0 @@
-# Used to access Spark across the VPN, since it's NAT'd
-alias pyspark='SPARK_LOCAL_IP=`ifconfig tun0 | grep '"'"'inet addr:'"'"' | cut -d: -f2 | awk '"'"'{ print $1}'"'"'` pyspark'
-alias spark-shell='SPARK_LOCAL_IP=`ifconfig tun0 | grep '"'"'inet addr:'"'"' | cut -d: -f2 | awk '"'"'{ print $1}'"'"'` spark-shell'
