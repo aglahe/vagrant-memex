19b488983a7453c38538ee2d214e61ab3db09070
Aaron Glahe <aaron.glahe@gmail.com>
2015-06-08 11:02:06 -0400

    Updated HBase, Hive and Imapla to new Zk cluster and various other -site changaes.  Fixed the \n in the basebox create script

--- basebox-create.sh
+++ basebox-create.sh
@@ -66,7 +66,7 @@ sudo mkdir -p /tmp/storm/local
 sudo wget http://www.scala-lang.org/files/archive/scala-2.11.5.tgz
 sudo /bin/tar xzvf scala-2.11.5.tgz -C /usr/lib
 sudo ln -s /usr/lib/scala-2.11.5 /usr/lib/scala
-sudo echo "export PATH=\$PATH:/usr/lib/scala/bin\nexport SCALA_HOME=/usr/lib/scala" >> /etc/profile.d/scala.sh
+sudo echo "export PATH=\$PATH:/usr/lib/scala/bin\\nexport SCALA_HOME=/usr/lib/scala" >> /etc/profile.d/scala.sh
 
 # Get MiniConda
 sudo wget http://repo.continuum.io/miniconda/Miniconda-3.7.0-Linux-x86_64.sh
