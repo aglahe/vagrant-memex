d5ef7c8cf012e7b926eb29a536ffa039f6105d1e
Annie Weng <annie.weng@l-3com.com>
2016-10-10 11:53:18 -0400

    add new vagrant box url and fix sudo issue with spark

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
-sudo alternatives --remove hadoop-conf /etc/hadoop/conf.xdata
 # create hadoop config files
 #remove existing one 
-sudo alternatives --remove hadoop-conf
+sudo alternatives --remove hadoop-conf /etc/hadoop/conf.xdata
 HDFS_COMPUTE_CONF=/etc/hadoop/conf.memex
 sudo alternatives --install /etc/hadoop/conf hadoop-conf $HDFS_COMPUTE_CONF 60
